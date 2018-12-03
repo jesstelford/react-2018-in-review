2a7c9f41c18c7178204e46b3fd98f4b8e44cbfe2
Jess Telford <jess+github@jes.st>
2018-12-03 22:51:31 +1100

    Build static next.js site for deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const { PHASE_PRODUCTION_SERVER } =
   process.env.NODE_ENV === 'development'
     ? {}
-    : require('next-server/constants');
+    : require('next/constants');
 
 module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_PRODUCTION_SERVER) {
