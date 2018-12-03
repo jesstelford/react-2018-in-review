663fce95a4388a2f9d82028a5401f5ddf27ebda9
Jess Telford <jess+github@jes.st>
2018-12-03 23:24:31 +1100

    Correct deployment magic for now

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const { PHASE_PRODUCTION_SERVER } =
   process.env.NODE_ENV === 'development'
     ? {}
-    : require('next/constants');
+    : require('next-server/constants');
 
 module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_PRODUCTION_SERVER) {
